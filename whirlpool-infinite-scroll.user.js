8c799e48c831945394e50042505f98459657748b
rf5860 <robert.foy@gmail.com>
2013-05-19 19:07:27 -0600

    Initial commit

---
+++ whirlpool-infinite-scroll.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name          Whirlpool Infinite Scroll
+// @description	  Add infinite scroll to all Whirlpool threads.
+// @include       http://forums.whirlpool.net.au/forum-replies*
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js
+// ==/UserScript==
+// Users
+var lastPage = null;
+var paging = false;
+$(window).scroll(function() {
+		     if($(window).scrollTop() + $(window).height() > $(document).height() - 500) {
+			 paging = true;
+			 var loc = window.location.href;
+			 var idx = loc.lastIndexOf("&p=");
+			 if (lastPage == null) {
+			     var page = idx == -1 ? 1 : loc.substring(loc.lastIndexOf("&p=") + 3);
+			     lastPage = page + 1;
+			 }
+			 var next = (idx == -1) ? loc : loc.substring(0, idx);
+			 next += "&p=" + lastPage++;
+			 $.get(next, 
+			       function(data) { 
+				   $('#replylist').append($('#replylist', data));
+				   paging = false;
+			       }
+			      );     
+		     }
+		 });
